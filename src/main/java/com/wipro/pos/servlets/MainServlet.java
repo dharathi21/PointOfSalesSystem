ea9f433c36060cab2353a7aeca8e0e57c2eb2062
dharathi21 <dharathi2108@gmail.com>
2026-01-06 20:55:26 +0530

    main servlet

--- src/main/java/com/wipro/pos/servlets/MainServlet.java
+++ src/main/java/com/wipro/pos/servlets/MainServlet.java
@@ -7,8 +7,11 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.sql.Date;
+import java.util.ArrayList;
+import java.util.List;
 
 import com.wipro.pos.bean.PosBean;
+import com.wipro.pos.dao.PosDAO;
 import com.wipro.pos.service.PosService;
 
 @WebServlet("/MainServlet")
@@ -28,8 +31,26 @@ public class MainServlet extends HttpServlet {
 		PosService posService=new PosService();
 		String result=posService.addRecord(bean);
 		return result;	
+	}
+	public PosBean viewRecord(HttpServletRequest request) {
+		PosBean bean=new PosBean();
+		PosService posService=new PosService();
+		String date=request.getParameter("date");
+		Date ogdate=Date.valueOf(date);
+		bean=posService.viewRecord(request.getParameter("customer"),ogdate);
+		return bean;
+		
+		
+	} 
+	public List<PosBean> viewAllRecords(HttpServletRequest request) {
+		List<PosBean> list=new ArrayList<PosBean>();
+		PosService posService=new PosService();
+		list=posService.viewAllRecords();
+		return list;
+		
 		
 	}
+
 
         
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
@@ -43,7 +64,33 @@ public class MainServlet extends HttpServlet {
 			}else {
 				response.sendRedirect("error.html");
 			}
-		}		
+		}
+		if("ViewRecord".equalsIgnoreCase(operation)) {
+			PosBean bean=new PosBean();
+			bean=viewRecord(request);
+			if(bean==null) {
+				request.setAttribute("message", "No matching records");
+				request.getRequestDispatcher("displayTransaction.jsp").forward(request, response);
+			}else {
+				request.setAttribute("bean", bean);
+				request.getRequestDispatcher("displayTransaction.jsp").forward(request, response);
+			}
+			
+		}
+		
+		if("viewAllRecords".equalsIgnoreCase(operation)) {
+			List<PosBean> list=new ArrayList<PosBean>();
+			list=viewAllRecords(request);
+			if(list==null) {
+				request.setAttribute("message", "No records available");
+				request.getRequestDispatcher("displayAllTransaction.jsp").forward(request, response);
+			}else {
+				request.setAttribute("bean", list);
+				request.getRequestDispatcher("displayAllTransaction.jsp").forward(request, response);
+			}	
+			
+		}
+		
 		}catch(Exception e) {
 			response.sendRedirect("error.html");
 		}
